5d8028660005d58879384922f634c5b2c4b2d7b1
Ryan Soto <rsoto003@ucr.edu>
2019-04-13 15:21:50 -0700

    added react router functionality to navbar and app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Data from './components/Data';
+import Info from './components/Info';
+import Landing from './components/Landing';
 
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Route path="/" exact component={Landing}/>
+          <Route path="/data" component={Data} />
+          <Route path="/info" component={Info} />
+
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Data.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Data extends Component {
+    render(){
+        return (
+            <h1 className="lead">Data Component</h1>
+        )
+    }
+}
+
+export default Data;
