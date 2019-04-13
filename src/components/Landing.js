abc55a10b71cabd6c3d6e8114bd3a8b9aca7694b
Ryan Soto <rsoto003@ucr.edu>
2019-04-13 15:49:37 -0700

    added image/layout to landing page.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import '../App.css';
+
 
 class Landing extends Component {
     render(){
         return ( 
-            <div ></div>
+            <div className="jumbotron jumbotron-image my-5 text-dark img-fluid">
+                <div className="jumbotron mt-3 text-center">
+                <h1 className="display-4">Count Your Calories!</h1>
+                <p className="lead">Keeping track of your dietary goals is never fun, but we make it easy for you.</p>
+                <hr className="my-4" />
+                <p>We help you control your diet, and meet your goals. </p>
+                <a className="btn btn-danger btn-lg mr-3" href="html/info.html" role="button">Get Started</a>
+                <a className="btn btn-dark btn-lg" href="html/data.html">Your Data</a>
+                </div>
+            </div>
         )
     }
 }
