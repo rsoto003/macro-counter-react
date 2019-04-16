0d7b3af4378fa3f63ebd12c9598eafc122ceae3e
Ryan Soto <rsoto003@ucr.edu>
2019-04-16 00:08:47 -0700

    adding server folder and initial setup with dependencies

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 6000;
+
