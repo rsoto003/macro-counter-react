56b3e1d4bd6591d8afe59d4102d3dda48fca9852
Ryan Soto <rsoto003@ucr.edu>
2019-04-16 00:24:15 -0700

    server is now running.

--- server/server.js
+++ server/server.js
@@ -5,3 +5,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = 6000;
 
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/macros', { useNewUrlParser: true} );
+const connection = mongoose.connection;
+
+connection.once('open', () => console.log(`MongoDB connection established successfully. Nice job.`));
+
+app.listen(PORT, () => console.log(`Server is up and running on port: ${PORT}`));
