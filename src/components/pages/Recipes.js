3855ad00e30f135e54c76cbbc1cf87e163bf153b
Ryan Soto <rsoto003@ucr.edu>
2020-05-25 22:26:19 -0700

    fixed card layout to account for different sized images, turned off autocomplete off on recipe form

--- src/components/pages/Recipes.js
+++ src/components/pages/Recipes.js
@@ -10,7 +10,7 @@ class Recipe extends Component{
     getRecipe = async e => {
         e.preventDefault();
         const API_KEY = '83f89a24bad24efc937b54a0af5fb5fa';
-        const searchRecipe = await axios.get(`https://api.spoonacular.com/recipes/search?apiKey=${API_KEY}&query=${this.state.recipe}`);
+        const searchRecipe = await axios.get(`https://api.spoonacular.com/recipes/search?apiKey=${API_KEY}&query=${this.state.recipe}&number=12`);
         const recipe_data = searchRecipe.data.results;
 
         console.log(recipe_data);
@@ -24,17 +24,17 @@ class Recipe extends Component{
     }
     render(){
         return (
-            <div>
-                <Form getRecipe={this.getRecipe} handleRecipeChange={this.handleRecipeChange} />
+            <div className="mt-5">
+                <Form getRecipe={this.getRecipe} handleRecipeChange={this.handleRecipeChange}/>
                 <div className="container">
-                <div className="row col-xl-12 mt-3">
+                <div className="row col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-3">
                     {this.state.recipes.map( recipe => {
-                    return <div className="col-md-6 col-lg-4 col-xl-4 col-sm-6 col-xs-6" key={recipe.id}>
+                    return <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6" key={recipe.id}>
                     <div className="card mb-3 text-dark">
-                    <img className="card-img-top" src={`https://spoonacular.com/cdn/ingredients_100x100/${recipe.image}`} alt={recipe.title}/>
+                    <img className="card-img-top" src={`https://spoonacular.com/recipeImages/${recipe.image}`} alt={recipe.title}/>
                         <div className="card-body">
                             <h5 className="card-title"> {recipe.title}</h5>
-                            <a className="btn btn-info" href={recipe.sourceUrl}>Recipe Me!</a>
+                            <a className="btn btn-info recipe-me-button" href={recipe.sourceUrl}>Recipe Me!</a>
                         </div>
                     </div>
                 </div> 
